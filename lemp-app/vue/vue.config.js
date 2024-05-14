70590dfc962c44713e84aecda5a2a7e2db041e81
Cbas420 <grepi.seti@gmail.com>
2024-05-14 18:55:21 +0200

    sa
    sa

--- lemp-app/vue/vue.config.js
+++ lemp-app/vue/vue.config.js
@@ -1,8 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  devServer: {
-    disableHostCheck: true // Habilitar esta opción para solucionar el error "Invalid Host header"
-  },
   transpileDependencies: true // Transpilar dependencias
 })
 // hola

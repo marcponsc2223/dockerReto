5a1f823d4798fd547748da336955e5ed2899ff6a
Cbas420 <grepi.seti@gmail.com>
2024-05-14 17:09:40 +0200

    s
    s

---
+++ lemp-app/src/resources/views/welcome.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Startup Genial</title>
+<style>
+  body, html {
+    height: 100%;
+    margin: 0;
+    font-family: 'Arial', sans-serif;
+  }
+  .hero {
+    background-color: #f06d06;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    flex-direction: column;
+    text-align: center;
+  }
+  h1 {
+    font-size: 50px;
+    margin-bottom: 20px;
+  }
+  p {
+    font-size: 20px;
+    margin-bottom: 30px;
+  }
+  .btn {
+    border: none;
+    padding: 10px 20px;
+    background-color: #fff;
+    color: #f06d06;
+    cursor: pointer;
+    font-size: 18px;
+    text-transform: uppercase;
+    letter-spacing: 2px;
+    transition: background-color 0.3s ease;
+  }
+  .btn:hover {
+    background-color: #e5e5e5;
+  }
+</style>
+</head>
+<body>
+
+<div class="hero">
+  <h1>¡Bienvenidos a Startup Politecnics ;)!</h1>
+  <p>La plataforma que impulsa tus ideas al éxito.</p>
+  <button class="btn">Explora Ahora</button>
+</div>
+
+</body>
+</html>
